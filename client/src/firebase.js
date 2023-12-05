6936e11d0d3eaa5e3ec7b8f61e8fe0a94d9b1853
Kin9989 <HaoQH@int.t4tek.co>
2023-12-05 09:52:11 +0700

    update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,14 +1,25 @@
 import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDCbSLqeYcpLbelOPrJVxmPQvAPnKUWy14",
+//   authDomain: "ecommerce-bdf98.firebaseapp.com",
+//   projectId: "ecommerce-bdf98",
+//   storageBucket: "ecommerce-bdf98.appspot.com",
+//   messagingSenderId: "171919899530",
+//   appId: "1:171919899530:web:ae4b7b58f17b7398e26673",
+//   measurementId: "G-4PV55SEFZ8"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDCbSLqeYcpLbelOPrJVxmPQvAPnKUWy14",
-  authDomain: "ecommerce-bdf98.firebaseapp.com",
-  projectId: "ecommerce-bdf98",
-  storageBucket: "ecommerce-bdf98.appspot.com",
-  messagingSenderId: "171919899530",
-  appId: "1:171919899530:web:ae4b7b58f17b7398e26673",
-  measurementId: "G-4PV55SEFZ8"
+  apiKey: "AIzaSyAPO6bUxTtMdxjJ-EpXVl4Ls7UEPmP5Muk",
+  authDomain: "web2023audio.firebaseapp.com",
+  databaseURL: "https://web2023audio-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web2023audio",
+  storageBucket: "web2023audio.appspot.com",
+  messagingSenderId: "243524202116",
+  appId: "1:243524202116:web:ad1f6e980baa56c3db1cd1",
+  measurementId: "G-TY1T09SQL5"
+
 };
 
 // Initialize Firebase
